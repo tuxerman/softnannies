614c24d4723e8bceef71ceb78a765bd431a45086
Robert Lychev <robertlychev@lawn-128-61-17-193.lawn.gatech.edu>
2014-04-21 14:12:16 -0400

    Updated port names

--- soft_nannies.py
+++ soft_nannies.py
@@ -1,5 +1,5 @@
 '''
-Georgia Tech CS6675 Spring 2014 Course Project: SoftNannies
+Georgia Tech CS6675 Spring 2014 Course Project: SoftNannies 
 Karim Habak
 Robert Lychev
 Sriram Padnamabhan
@@ -17,16 +17,16 @@ import os
 import csv
 
 #list of websites/ip addresses that should not be accessed by certain MAC addresses outside a certain range of time
-access_policies = "%s/pyretic/pyretic/examples/soft_nannies/access_policies.csv" % os.environ[ 'HOME' ]
+access_policies = "%s/pyretic/pyretic/examples/soft_nannies/access_policies.csv" % os.environ[ 'HOME' ] 
 
-#list of mac addresses and their corresponding forwarding port numbers
-access_configuration= "%s/pyretic/pyretic/examples/soft_nannies/access_configuration.csv" % os.environ[ 'HOME' ]
+#list of mac addresses and their corresponding forwarding port numbers 
+access_configuration= "%s/pyretic/pyretic/examples/soft_nannies/portData.csv" % os.environ[ 'HOME' ] 
 
 #if true, then anyone can joing the network, otherwise only the devices with specific MACs as listed in policy_out.csv can
 access_mode = "%s/pyretic/pyretic/examples/soft_nannies/access_mode.csv" % os.environ[ 'HOME' ]
 
 #a tuple for holding network configuration, i.e. forwarding port numbers for each host in the networks
-ACCESS_CONFIGURATION = namedtuple('ACCESS_CONFIGURATION', ('mac', 'out_port', 'in_port', 'tc'))
+ACCESS_CONFIGURATION = namedtuple('ACCESS_CONFIGURATION', ('mac', 'tc', 'port_down', 'port_up'))
 #a tuple for holding access policies, i.e. which hosts in the network are restricted from accessing which sites
 ACCESS_POLICY = namedtuple('ACCESS_POLICY', ('src_mac', 'dst_ip', 't_begin', 't_end'))
 
@@ -34,50 +34,50 @@ ACCESS_POLICY = namedtuple('ACCESS_POLICY', ('src_mac', 'dst_ip', 't_begin', 't_
 #create the default switching behavior for the double switch architecture
 #@dynamic here means that the function below will create a new dynamic
 #policy class with the name "double_trouble"
-@dynamic
+@dynamic   
 def double_trouble(self):
 
-
+    
     #this will be static;
     #these are ids of the vswitches
     self.down_switch = 1 #int("0x0000080027b1d7f1",16)
-    self.up_switch = 2 #int("0x0000080027b7bf8b",16)
-
-    #figure out if all packets should be flooded or dropped initially by default
+    self.up_switch = 2 #int("0x0000080027b7bf8b",16) 
+   
+    #figure out if all packets should be flooded or dropped initially by default 
     with open(access_mode, 'r') as a_mode:
         reader = DictReader(a_mode, delimiter = ",")
         access_m = {}
         for row in reader:
             access_m[row['id']] = row['value']
-
+    
     if(int(access_m['1']) == 1):
-        self.frwrd = flood() #fwd(6633);
+	self.frwrd = flood() #fwd(6633); 
     else:
-        self.frwrd = drop #very strict policy
+	self.frwrd = drop #very strict policy
 
     #default query for receiving incoming packets
     self.qry = packets(limit=1,group_by=['srcmac','switch'])
-
+  
     #set the initial internal policy value
     self.policy = self.frwrd + self.qry
 
-
+	
     #update the forwarding configuration from the access configuration policy file
     with open(access_configuration, 'r') as c_file:
-        reader = DictReader(c_file, delimiter = ",")
+   	reader = DictReader(c_file, delimiter = ",")
         access_c = {}
         for row in reader:
-            access_c[row['id']] = ACCESS_CONFIGURATION(row['mac'], row['out_port'], row['in_port'], row['tc'])
+	    access_c[row['id']] = ACCESS_CONFIGURATION(row['mac'], row['tc'], row['port_down'], row['port_up'])
     for policy in access_c.itervalues():
-        self.frwrd = if_(match(srcmac=MAC(policy.mac),switch=self.down_switch),fwd(int(policy.out_port)),self.policy)
-        self.policy = self.frwrd + self.qry
-        self.frwrd = if_(match(dstmac=MAC(policy.mac),switch=self.up_switch),fwd(int(policy.in_port)),self.policy)
-        self.policy = self.frwrd + self.qry
+ 	self.frwrd = if_(match(srcmac=MAC(policy.mac),switch=self.down_switch),fwd(int(policy.port_down)),self.policy)
+    	self.policy = self.frwrd + self.qry
+	self.frwrd = if_(match(dstmac=MAC(policy.mac),switch=self.up_switch),fwd(int(policy.port_up)),self.policy)
+    	self.policy = self.frwrd + self.qry
 
     #the following logc was borrowed from the pyretic_switch module
     #the idea is to take each new packet pkt and update the forwarding policy
-    #so that subsequent incoming packets on this switch whose dstmac matches pkt's srcmac
-    #(accessed like in a dictionary pkt['srcmac']) will be forwarded out  pkt's inport
+    #so that subsequent incoming packets on this switch whose dstmac matches pkt's srcmac 
+    #(accessed like in a dictionary pkt['srcmac']) will be forwarded out  pkt's inport 
     #(pyretic packets are located, so this value is accessed just like srcmac,i.e., p['inport'])
     def learn_from_a_packet(pkt):
         #set the forwarding policy
@@ -86,7 +86,7 @@ def double_trouble(self):
                            self.policy)
         #update the overall policy
         self.policy = self.frwrd + self.qry
-        #print self.policy
+        #print self.policy 
 
     #learn_from_a_packet is called back every time our query sees a new packet
     self.qry.register_callback(learn_from_a_packet)
@@ -104,12 +104,12 @@ def main():
 
     #read in the access policies and update them
     with open(access_policies, 'r') as p_file:
-        reader = DictReader(p_file, delimiter = ",")
+   	reader = DictReader(p_file, delimiter = ",")
         access_p = {}
         for row in reader:
-            access_p[row['id']] = ACCESS_POLICY(row['src_mac'], row['dst_ip'], row['t_begin'], row['t_end'])
+	    access_p[row['id']] = ACCESS_POLICY(row['src_mac'], row['dst_ip'], row['t_begin'], row['t_end'])
     for policy in access_p.itervalues():
-        not_allowed = not_allowed | match(srcmac=MAC(policy.src_mac),dstip=IP(policy.dst_ip)) | match(srcip=IP(policy.dst_ip),dstmac=MAC(policy.src_mac))
+	not_allowed = not_allowed | match(srcmac=MAC(policy.src_mac),dstip=IP(policy.dst_ip)) | match(srcip=IP(policy.dst_ip),dstmac=MAC(policy.src_mac))
 
     #express allowed packets as the complement of not_allowed
     allowed = ~not_allowed
